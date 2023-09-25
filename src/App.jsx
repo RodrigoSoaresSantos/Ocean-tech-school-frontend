e9df956ae85d59e7adde32345eb1480fea62483a
RodrigoSoaresSantos <145621491+RodrigoSoaresSantos@users.noreply.github.com>
2023-09-25 00:03:05 +0000

    Exercicio frontend

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ function App() {
   return (
     <>
       <div className="card">
-        <h2>Rick Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+        <h2>Taylor Swift</h2>
+        <tag>World Tour</tag>
+        <tag>No more tickets</tag>
+        <p><tag>Available</tag></p>
+        <p></p>
+        <img src="https://upload.wikimedia.org/wikipedia/pt/f/fb/The_Eras_Tour.jpg" />
+        
       </div>
     </>
   )
