9387f26c1dd83ec3940d46211edaf122ae1be9d1
RodrigoSoaresSantos <145621491+RodrigoSoaresSantos@users.noreply.github.com>
2023-09-26 01:12:38 +0000

    Carregamento dinâmico

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,16 @@
 import "./Card.css"
 
-export default function Card() {
-    return <div>Card</div>
+export default function Card(props) {
+    console.log(props);
+    const item = props.item;
+    
+    return <div className="card">
+    <h2>{item.nome}</h2>
+    <tag>World Tour</tag>
+    <tag>No more tickets</tag>
+    <p><tag>Available</tag></p>
+    <p></p>
+    <img src={item.imagemUrl} />
+    
+  </div>
 }
